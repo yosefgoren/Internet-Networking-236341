8cf0d629fcd787db8e2ff6ba3b1aee043f2890e0
yosef <yosefisraelgoren@gmail.com>
2023-06-24 21:37:36 +0300

    added initial report file

--- Homework4/loadBalancer/Emulator/Emulator.cpp
+++ Homework4/loadBalancer/Emulator/Emulator.cpp
@@ -1,28 +1,27 @@
 #include <vector>
 #include <map>
+#include <memory>
 #include <functional>
 #include "../Common/Request.hpp"
+#include "../LBCore/LBCore.hpp"
 
 class Emulator{
 public:
     static constexpr unsigned NUM_CLIENTS = 5;
     typedef std::function<void()> Event;
     
-    Emulator()
-            :lb(*dispatchLBCore(argc, argv)){
+    Emulator(int argc, char** argv)
+            :lb(dispatchLBCore(argc, argv)){
         worker_infos = get_server_infos();
-        for(unsigned client_idx = NUM_CLIENTS-1; client_idx >= 0; --client_idx){
-                std::string client_requests_filename = "h"+std::to_string(client_idx)+".in";
-            client_requests_lists.push_back(parseRequestsFile(client_requests_filename));
+        for(unsigned client_idx = NUM_CLIENTS; client_idx > 0; client_idx--){
+            std::string client_requests_filename = "h"+std::to_string(client_idx)+".in";
+            printf("parsing requests file named '%s'.\n", client_requests_filename.c_str());
+            client_requests_lists.push_back(Request::parseRequestsFile(client_requests_filename));
         }
         workers_times_to_finish = {0, 0, 0};
 
         for(unsigned client_idx = 0; client_idx < NUM_CLIENTS; ++client_idx){
-            std::vector<Request>& req_list = client_requests_lists[client_idx];
-            if(req_list.size() > 0){
-                Request& first_request = req_list[0];
-                event_map.emplace(0, createClientRequestEvent(client_idx));
-            }
+            event_map.emplace(0, createClientRequestEvent(client_idx));
         }
     }
 
@@ -31,7 +30,11 @@ public:
             Event& event = event_map.begin()->second;
             event();
         }
-        return std::max(worker_total_times);
+        int m = 0;
+        for(int time: worker_total_times){
+            m = m > time ? m : time;
+        }
+        return m;
     }
     
     Event createClientRequestEvent(int client_idx){
@@ -39,8 +42,8 @@ public:
             auto& client_requests_list = client_requests_lists[client_idx];
             Request next_client_request = client_requests_list.back();
             client_requests_list.pop_back();
-            int server_idx = lb.handleRequest(next_client_request.to_string());
-            int elapsed_time = worker_infos[server_idx].getCompletionTime(next_client_request);
+            int server_idx = lb->handleRequest(next_client_request.to_string().c_str());
+            int elapsed_time = worker_infos[server_idx]->getCompletionTime(next_client_request);
             
             workers_times_to_finish[server_idx] += elapsed_time;
             event_map.emplace(workers_times_to_finish[server_idx], createServerResponseEvent(client_idx, server_idx, elapsed_time));
@@ -55,11 +58,8 @@ public:
         };
     }
 
-
-
     std::map<unsigned, Event> event_map;
-
-    LBCore& lb;
+    std::shared_ptr<LBCore> lb;
     std::vector<std::shared_ptr<WorkerServerInfo>> worker_infos;
     std::vector<std::vector<Request>> client_requests_lists;
     std::vector<unsigned> workers_times_to_finish;
@@ -67,6 +67,6 @@ public:
 };
 
 int main(int argc, char** argv){
-    Emulator e();
+    Emulator e(argc, argv);
     printf("total time is: %d\n", e.run());
 }
